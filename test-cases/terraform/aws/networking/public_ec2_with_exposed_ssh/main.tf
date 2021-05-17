9814f3e1cd82822d25123e048dbe64970e21319e
Christophe Tafani-Dereeper <christophe@tafani-dereeper.me>
2021-05-17 21:28:54 +0200

    Add test case for EC2 instance with public IP and open SSH

---
+++ test-cases/terraform/aws/networking/public_ec2_with_exposed_ssh/main.tf
@@ -0,0 +1,80 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+locals {
+  test_description = "An EC2 instance has a public IP assigned and a security group allowing inbound access to port 22"
+  test_name        = "Public EC2 instance with SSH exposed"
+}
+
+resource "aws_vpc" "nondefault" {
+  cidr_block = "10.1.1.0/24"
+}
+
+resource "aws_network_acl" "ec2_nacl" {
+  vpc_id = aws_vpc.nondefault.id
+  subnet_ids = [aws_subnet.nondefault_1.id]
+
+  egress {
+    protocol   = -1
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65000
+  }
+
+  ingress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65000
+  }
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "nondefault_1" {
+  vpc_id = aws_vpc.nondefault.id
+  cidr_block = "10.1.1.128/25"
+  availability_zone = "eu-central-1a"
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.nondefault.id
+}
+
+resource aws_route_table "nondefault_1" {
+  vpc_id = aws_vpc.nondefault.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "nondefault_1" {
+  route_table_id = aws_route_table.nondefault_1.id
+  subnet_id = aws_subnet.nondefault_1.id
+}
+
+resource "aws_security_group" "publicly_accessible_sg" {
+  vpc_id = aws_vpc.nondefault.id
+  ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "public_ins" {
+  ami = "ami-0130bec6e5047f596"
+  instance_type = "t3.nano"
+  associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.publicly_accessible_sg.id]
+  subnet_id = aws_subnet.nondefault_1.id
+}
